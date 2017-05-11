d6960f7a51a53f216cb2d611c42c8778edd03f64
Anna <dffh@mail.ru>
2017-05-11 17:26:27 +0700

    ethjetd

---
+++ lights.js
@@ -0,0 +1,28 @@
+$(function(){
+	var status=false;
+	var update=function() {
+		if (status) {
+			$('#red').removeClass('red');	
+			$('#green').addClass('green');	
+		} else {
+			$('#red').addClass('red');	
+			$('#green').removeClass('green');
+		}
+		$('#yellow').removeClass('yellow');	
+	};
+	function setYellow() {
+		$('#yellow').addClass('yellow');
+		$('#green').removeClass('green');
+	}	 
+	setInterval(function () {
+		status=!status;
+		update();
+	}, 5000);
+	setTimeout(function() {
+		setInterval(function() {
+			setYellow();		
+		}, 5000);
+		setYellow();	
+	},4500)
+	update();
+});
